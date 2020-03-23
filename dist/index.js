178b6fd64655190087ba2aca2bc4ac7dc4858e45
pa <pa@kabelkultur.se>
2020-03-23 15:33:28 +0100

    new build

--- dist/index.js
+++ dist/index.js
@@ -404,7 +404,7 @@ ReactWizard.propTypes = {
   steps: _propTypes2.default.arrayOf(_propTypes2.default.shape({
     stepName: _propTypes2.default.string.isRequired,
     stepIcon: _propTypes2.default.string,
-    component: _propTypes2.default.func.isRequired,
+    component: _propTypes2.default.oneOf([_propTypes2.default.func.isRequired, _propTypes2.default.object.isRequired]),
     stepProps: _propTypes2.default.object
   })).isRequired
 };
