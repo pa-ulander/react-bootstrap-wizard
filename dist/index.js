33affe0d179f9ec0bd3188516469ed61a0f78c15
pa <pa@kabelkultur.se>
2020-03-23 13:21:36 +0100

    wip fix redux compatibility, new build

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 Object.defineProperty(exports, "__esModule", {
   value: true
@@ -8,17 +8,17 @@ var _extends = Object.assign || function (target) { for (var i = 1; i < argument
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-var _react = require("react");
+var _react = require('react');
 
 var _react2 = _interopRequireDefault(_react);
 
-var _reactstrap = require("reactstrap");
+var _reactstrap = require('reactstrap');
 
-var _propTypes = require("prop-types");
+var _propTypes = require('prop-types');
 
 var _propTypes2 = _interopRequireDefault(_propTypes);
 
-var _classnames4 = require("classnames");
+var _classnames4 = require('classnames');
 
 var _classnames5 = _interopRequireDefault(_classnames4);
 
@@ -42,36 +42,36 @@ var ReactWizard = function (_React$Component) {
 
     var width = void 0;
     if (_this.props.steps.length === 1) {
-      width = "100%";
+      width = '100%';
     } else {
       if (window.innerWidth < 600) {
         if (_this.props.steps.length !== 3) {
-          width = "50%";
+          width = '50%';
         } else {
-          width = 100 / 3 + "%";
+          width = 100 / 3 + '%';
         }
       } else {
         if (_this.props.steps.length === 2) {
-          width = "50%";
+          width = '50%';
         } else {
-          width = 100 / 3 + "%";
+          width = 100 / 3 + '%';
         }
       }
     }
     _this.state = {
       currentStep: 0,
       highestStep: 0,
-      color: _this.props.color !== undefined ? _this.props.color : "primary",
+      color: _this.props.color !== undefined ? _this.props.color : 'primary',
       nextButton: _this.props.steps.length > 1 ? true : false,
       previousButton: false,
       finishButton: _this.props.steps.length === 1 ? true : false,
       width: width,
       wizardData: _this.props.wizardData !== undefined ? _this.props.wizardData : {},
       movingTabStyle: {
-        transition: "transform 0s"
+        transition: 'transform 0s'
       },
       progressbarStyle: {
-        width: 100 / _this.props.steps.length / 2 + "%"
+        width: 100 / _this.props.steps.length / 2 + '%'
       }
     };
     _this.navigationStepChange = _this.navigationStepChange.bind(_this);
@@ -83,23 +83,23 @@ var ReactWizard = function (_React$Component) {
   }
 
   _createClass(ReactWizard, [{
-    key: "componentDidMount",
+    key: 'componentDidMount',
     value: function componentDidMount() {
       this.refreshAnimation(0);
-      window.addEventListener("resize", this.updateWidth.bind(this));
+      window.addEventListener('resize', this.updateWidth.bind(this));
     }
   }, {
-    key: "componentWillUnmount",
+    key: 'componentWillUnmount',
     value: function componentWillUnmount() {
       this.isCancelled = true;
-      window.removeEventListener("resize", this.updateWidth);
+      window.removeEventListener('resize', this.updateWidth);
       var id = window.setTimeout(null, 0);
       while (id--) {
         window.clearTimeout(id);
       }
     }
   }, {
-    key: "updateWidth",
+    key: 'updateWidth',
     value: function updateWidth() {
       var _this2 = this;
 
@@ -108,7 +108,7 @@ var ReactWizard = function (_React$Component) {
       }, 200);
     }
   }, {
-    key: "navigationStepChange",
+    key: 'navigationStepChange',
     value: function navigationStepChange(key) {
       if (this.props.navSteps) {
         var validationState = true;
@@ -134,14 +134,15 @@ var ReactWizard = function (_React$Component) {
       }
     }
   }, {
-    key: "nextButtonClick",
+    key: 'nextButtonClick',
     value: function nextButtonClick() {
-      if (this.props.validate && (this.refs[this.props.steps[this.state.currentStep].stepName].isValidated !== undefined && this.refs[this.props.steps[this.state.currentStep].stepName].isValidated() || this.refs[this.props.steps[this.state.currentStep].stepName].isValidated === undefined) || this.props.validate === undefined || !this.props.validate) {
+      var wrappedInst = this.refs[this.props.steps[this.state.currentStep].stepName].getWrappedInstance();
+
+      if (this.props.validate && (wrappedInst.isValidated !== undefined && wrappedInst.isValidated() || wrappedInst.isValidated === undefined) || this.props.validate === undefined || !this.props.validate) {
         var key = this.state.currentStep + 1;
         this.setState({
-          wizardData: _extends({}, this.state.wizardData, _defineProperty({}, this.props.steps[this.state.currentStep].stepName, this.refs[this.props.steps[this.state.currentStep].stepName].state)),
+          wizardData: _extends({}, this.state.wizardData, this.refs[this.props.steps[this.state.currentStep].stepName].state),
           currentStep: key,
-          highestStep: key > this.state.highestStep ? key : this.state.highestStep,
           nextButton: this.props.steps.length > key + 1 ? true : false,
           previousButton: key > 0 ? true : false,
           finishButton: this.props.steps.length === key + 1 ? true : false
@@ -150,7 +151,7 @@ var ReactWizard = function (_React$Component) {
       }
     }
   }, {
-    key: "previousButtonClick",
+    key: 'previousButtonClick',
     value: function previousButtonClick() {
       var key = this.state.currentStep - 1;
       if (key >= 0) {
@@ -166,14 +167,14 @@ var ReactWizard = function (_React$Component) {
       }
     }
   }, {
-    key: "finishButtonClick",
+    key: 'finishButtonClick',
     value: function finishButtonClick() {
       var _this3 = this;
 
       if (this.props.validate === false && this.props.finishButtonClick !== undefined || this.props.validate && (this.refs[this.props.steps[this.state.currentStep].stepName].isValidated !== undefined && this.refs[this.props.steps[this.state.currentStep].stepName].isValidated() || this.refs[this.props.steps[this.state.currentStep].stepName].isValidated === undefined) && this.props.finishButtonClick !== undefined) {
         this.setState({
           progressbarStyle: {
-            width: "100%"
+            width: '100%'
           },
           wizardData: _extends({}, this.state.wizardData, _defineProperty({}, this.props.steps[this.state.currentStep].stepName, this.refs[this.props.steps[this.state.currentStep].stepName].state))
         }, function () {
@@ -182,7 +183,7 @@ var ReactWizard = function (_React$Component) {
       }
     }
   }, {
-    key: "refreshAnimation",
+    key: 'refreshAnimation',
     value: function refreshAnimation(index) {
       var total = this.props.steps.length;
       var li_width = 100 / total;
@@ -200,7 +201,7 @@ var ReactWizard = function (_React$Component) {
         li_width = 50;
       }
 
-      this.setState({ width: li_width + "%" });
+      this.setState({ width: li_width + '%' });
 
       var step_width = move_distance;
 
@@ -213,8 +214,8 @@ var ReactWizard = function (_React$Component) {
 
       var movingTabStyle = {
         width: step_width,
-        transform: "translate3d(" + move_distance + "px, " + vertical_level + "px, 0)",
-        transition: "all 0.5s cubic-bezier(0.29, 1.42, 0.79, 1)"
+        transform: 'translate3d(' + move_distance + 'px, ' + vertical_level + 'px, 0)',
+        transition: 'all 0.5s cubic-bezier(0.29, 1.42, 0.79, 1)'
       };
       this.setState({
         movingTabStyle: movingTabStyle,
@@ -224,41 +225,41 @@ var ReactWizard = function (_React$Component) {
       });
     }
   }, {
-    key: "render",
+    key: 'render',
     value: function render() {
       var _this4 = this;
 
       return _react2.default.createElement(
-        "div",
-        { className: "wizard-container", ref: "wizard" },
+        'div',
+        { className: 'wizard-container', ref: 'wizard' },
         _react2.default.createElement(
           _reactstrap.Card,
-          { className: "card card-wizard active", "data-color": this.state.color },
+          { className: 'card card-wizard active', 'data-color': this.state.color },
           this.props.title !== undefined || this.props.description !== undefined ? _react2.default.createElement(
             _reactstrap.CardHeader,
             {
-              className: this.props.headerTextCenter !== undefined ? "text-center" : "",
-              "data-background-color": this.state.color
+              className: this.props.headerTextCenter !== undefined ? 'text-center' : '',
+              'data-background-color': this.state.color
             },
             this.props.title !== undefined ? _react2.default.createElement(
               _reactstrap.CardTitle,
-              { tag: "h3" },
+              { tag: 'h3' },
               this.props.title
             ) : null,
             this.props.description !== undefined ? _react2.default.createElement(
-              "h3",
-              { className: "description" },
+              'h3',
+              { className: 'description' },
               this.props.description
             ) : null,
             _react2.default.createElement(
-              "div",
-              { className: "wizard-navigation", ref: "navStepsLi" },
+              'div',
+              { className: 'wizard-navigation', ref: 'navStepsLi' },
               _react2.default.createElement(
-                "div",
-                { className: "progress-with-circle" },
-                _react2.default.createElement("div", {
-                  className: "progress-bar",
-                  role: "progressbar",
+                'div',
+                { className: 'progress-with-circle' },
+                _react2.default.createElement('div', {
+                  className: 'progress-bar',
+                  role: 'progressbar',
                   style: this.state.progressbarStyle
                 })
               ),
@@ -277,9 +278,9 @@ var ReactWizard = function (_React$Component) {
                           return _this4.navigationStepChange(key);
                         }
                       },
-                      prop.stepIcon !== undefined && prop.stepIcon !== "" ? _react2.default.createElement("i", { className: prop.stepIcon }) : null,
+                      prop.stepIcon !== undefined && prop.stepIcon !== '' ? _react2.default.createElement('i', { className: prop.stepIcon }) : null,
                       _this4.props.progressbar ? _react2.default.createElement(
-                        "p",
+                        'p',
                         null,
                         prop.stepName
                       ) : prop.stepName
@@ -288,12 +289,12 @@ var ReactWizard = function (_React$Component) {
                 })
               ),
               this.props.progressbar ? null : _react2.default.createElement(
-                "div",
-                { className: "moving-tab", style: this.state.movingTabStyle },
-                this.props.steps[this.state.currentStep].stepIcon !== undefined && this.props.steps[this.state.currentStep].stepIcon !== "" ? _react2.default.createElement("i", {
+                'div',
+                { className: 'moving-tab', style: this.state.movingTabStyle },
+                this.props.steps[this.state.currentStep].stepIcon !== undefined && this.props.steps[this.state.currentStep].stepIcon !== '' ? _react2.default.createElement('i', {
                   className: this.props.steps[this.state.currentStep].stepIcon
                 }) : null,
-                "\u25B2"
+                '\u25B2'
               )
             )
           ) : null,
@@ -309,15 +310,15 @@ var ReactWizard = function (_React$Component) {
                   {
                     tabId: key,
                     key: key,
-                    className: (0, _classnames5.default)("fade", {
+                    className: (0, _classnames5.default)('fade', {
                       show: _this4.state.currentStep === key
                     })
                   },
-                  typeof prop.component === "function" ? _react2.default.createElement(prop.component, _extends({
+                  typeof prop.component === 'function' ? _react2.default.createElement(prop.component, _extends({
                     ref: prop.stepName,
                     wizardData: _this4.state.wizardData
                   }, prop.stepProps)) : _react2.default.createElement(
-                    "div",
+                    'div',
                     { ref: prop.stepName },
                     prop.component
                   )
@@ -329,44 +330,44 @@ var ReactWizard = function (_React$Component) {
             _reactstrap.CardFooter,
             null,
             _react2.default.createElement(
-              "div",
-              { style: { float: "right" } },
+              'div',
+              { style: { float: 'right' } },
               this.state.nextButton ? _react2.default.createElement(
                 _reactstrap.Button,
                 {
-                  className: (0, _classnames5.default)("btn-next", _defineProperty({}, this.props.nextButtonClasses, this.props.nextButtonClasses !== undefined)),
+                  className: (0, _classnames5.default)('btn-next', _defineProperty({}, this.props.nextButtonClasses, this.props.nextButtonClasses !== undefined)),
                   onClick: function onClick() {
                     return _this4.nextButtonClick();
                   }
                 },
-                this.props.nextButtonText !== undefined ? this.props.nextButtonText : "Next"
+                this.props.nextButtonText !== undefined ? this.props.nextButtonText : 'Next'
               ) : null,
               this.state.finishButton ? _react2.default.createElement(
                 _reactstrap.Button,
                 {
-                  className: (0, _classnames5.default)("btn-finish d-inline-block", _defineProperty({}, this.props.finishButtonClasses, this.props.finishButtonClasses !== undefined)),
+                  className: (0, _classnames5.default)('btn-finish d-inline-block', _defineProperty({}, this.props.finishButtonClasses, this.props.finishButtonClasses !== undefined)),
                   onClick: function onClick() {
                     return _this4.finishButtonClick();
                   }
                 },
-                this.props.finishButtonText !== undefined ? this.props.finishButtonText : "Finish"
+                this.props.finishButtonText !== undefined ? this.props.finishButtonText : 'Finish'
               ) : null
             ),
             _react2.default.createElement(
-              "div",
-              { style: { float: "left" } },
+              'div',
+              { style: { float: 'left' } },
               this.state.previousButton ? _react2.default.createElement(
                 _reactstrap.Button,
                 {
-                  className: (0, _classnames5.default)("btn-previous", _defineProperty({}, this.props.previousButtonClasses, this.props.previousButtonClasses !== undefined)),
+                  className: (0, _classnames5.default)('btn-previous', _defineProperty({}, this.props.previousButtonClasses, this.props.previousButtonClasses !== undefined)),
                   onClick: function onClick() {
                     return _this4.previousButtonClick();
                   }
                 },
-                this.props.previousButtonText !== undefined ? this.props.previousButtonText : "Previous"
+                this.props.previousButtonText !== undefined ? this.props.previousButtonText : 'Previous'
               ) : null
             ),
-            _react2.default.createElement("div", { className: "clearfix" })
+            _react2.default.createElement('div', { className: 'clearfix' })
           )
         )
       );
@@ -378,15 +379,15 @@ var ReactWizard = function (_React$Component) {
 
 ReactWizard.defaultProps = {
   validate: false,
-  previousButtonText: "Previous",
-  finishButtonText: "Finish",
-  nextButtonText: "Next",
-  color: "primary",
+  previousButtonText: 'Previous',
+  finishButtonText: 'Finish',
+  nextButtonText: 'Next',
+  color: 'primary',
   progressbar: false
 };
 
 ReactWizard.propTypes = {
-  color: _propTypes2.default.oneOf(["primary", "green", "orange", "red", "blue"]),
+  color: _propTypes2.default.oneOf(['primary', 'green', 'orange', 'red', 'blue']),
   previousButtonClasses: _propTypes2.default.string,
   finishButtonClasses: _propTypes2.default.string,
   nextButtonClasses: _propTypes2.default.string,
